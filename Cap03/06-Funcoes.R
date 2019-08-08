2ec800b02d70e79582fa45a675a3c8bec8410430
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-08-07 21:39:03 -0300

    Funcoes

--- Cap03/06-Funcoes.R
+++ Cap03/06-Funcoes.R
@@ -12,3 +12,24 @@ append(c(1:5),6)
 
 # Funções dentro de funções
 plot(rnorm(10))
+
+# Criando funções
+myFunc <- function(x) {
+  x + x
+}
+myFunc(10)
+
+myFunc2 <- function(a, b) {
+  valor = a ^ b
+  print(valor)
+}
+
+myFunc2(2,2)
+
+jogando_dados <- function() {
+  num <- sample(1:6, size = 1)
+  num
+}
+jogando_dados()
+
+# Funções sem número definido de argumentos
