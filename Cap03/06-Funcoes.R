ffd44669ca38ec27ac18bee9da46ff66bedbb44b
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-08-07 21:35:51 -0300

    Funcoes

--- Cap03/06-Funcoes.R
+++ Cap03/06-Funcoes.R
@@ -1,2 +1,14 @@
 ?sample
 
+args(sample)
+args(mean)
+args(sd)
+
+# Funções built-in (pacote base do pacote R)
+abs(-43)
+sum(c(1:5))
+mean(c(1:5))
+append(c(1:5),6)
+
+# Funções dentro de funções
+plot(rnorm(10))
