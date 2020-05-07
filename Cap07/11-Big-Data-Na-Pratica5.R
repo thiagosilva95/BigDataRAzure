e16fef40f2e5dfb934d954716dc6c34d35f34187
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-05-06 21:59:56 -0300

    Big Data na Prática - Análise de Séries Temporais no Mercado Financeiro

---
+++ Cap07/11-Big-Data-Na-Pratica5.R
@@ -0,0 +1,15 @@
+# Big Data na Prática - Análise de Séries Temporais no Mercado Financeiro
+
+setwd("D:/codes/R/dsa/BigDataRAzure/Cap07")
+getwd()
+
+# http://www.quantmod.com
+
+# Instalar e carregar os pacotes
+install.packages("quantmod") # Modelagem financeira quantitativa
+install.packages("xts")
+install.packages("moments")
+library(quantmod)
+library(xts)
+library(moments)
+
