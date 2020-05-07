4f21ea9cc96442f08f25cc6454b91f77ae149371
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-05-06 21:44:08 -0300

    Estudo de Caso - Extraindo  Dados da Web com Web Scraping em R

--- Cap07/09-Web_Scraping.R
+++ Cap07/09-Web_Scraping.R
@@ -1,4 +1,76 @@
 # Estudo de Caso - Extraindo  Dados da Web com Web Scraping em R
 
+# Web Crawling - "rastejar" por uma web page ou site buscando dados
+# Web Scraping - "raspar" os dados de uma web page
+
+
 setwd("D:/codes/R/dsa/BigDataRAzure/Cap07")
 getwd()
+
+# https://www.nytimes.com/interactive/2017/06/23/opinion/trumps-lies.html
+
+install.packages("rvest")
+library(rvest)
+
+library(stringr)
+library(dplyr)
+library(lubridate)
+library(readr)
+
+# Leitura da web page - Retorna um documento xml
+webpage <- read_html("https://www.nytimes.com/interactive/2017/06/23/opinion/trumps-lies.html")
+webpage
+class(webpage)
+
+# Extraindo os registros
+# Cada elemento na web page acima tem o seguinte formato em html:
+# <span class="short-desc"><strong> DATE </strong> LIE <span class="short-truth"><a href="URL"> EXPLANATION </a></span></span>
+?html_nodes
+results <- webpage %>% html_nodes(".short-desc")
+results
+class(results)
+
+# Construindo o dataset
+records <- vector("list", length = length(results))
+records
+
+for (i in seq_along(results)) {
+  date <- str_c(results[i] %>% 
+                  html_nodes("strong") %>%
+                  html_text(trim = TRUE), ', 2017')
+  lie <- str_sub(xml_contents(results[i])[2] %>% html_text(trim = TRUE), 2, -2)
+  explanation <- str_sub(results[i] %>% 
+                           html_nodes(".short-truth") %>%
+                           html_text(trim = TRUE), 2, -2)
+  url <- results[i] %>% html_nodes("a") %>% html_attr("href")
+  records[[i]] <- data.frame(date = date, lie = lie, explanation = explanation, url = url)
+}
+
+# Dataset final
+df <- bind_rows(records)
+View(df)
+
+# Transformando o campo data para o formato Date em R
+df$date <- mdy(df$date)
+
+# Exportando para csv
+write_csv(df, "mentiras_trump.csv")
+
+# Lendo os dados
+df <-  read_csv("mentiras_trump.csv")
+View(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
